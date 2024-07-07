6a0d44897db41b03dff115399e43753b3d9fd86d
18612295553 <morefreeze@gmail.com>
2024-07-07 22:14:38 +0800

    print future 30 days

--- every_day.py
+++ every_day.py
@@ -1,12 +1,15 @@
 import datetime
 from tqdm import tqdm
 from calendar_puzzle.board import Game
+from calendar_puzzle.dancing_link.calendar import FasterGame
 from calendar_puzzle.shape import Shape, ShapeS, ShapeN
 
 if __name__ == '__main__':
     dt = datetime.date.today()
-    for i in tqdm(range(365)):
-        g = Game(dt=dt)
-        dt += datetime.timedelta(days=1)
+    for i in tqdm(range(30)):
+        # g = Game(dt=dt)
+        g = FasterGame(dt=dt)
         # print(g.board)
         g.solve(find_one_exit=True)
+        print(dt)
+        dt += datetime.timedelta(days=1)
